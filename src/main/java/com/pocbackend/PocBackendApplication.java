89b4da1fc182a2619cd4af3e25e5558936a0f5a5
petprojectsindia <117950491+petprojectsindia@users.noreply.github.com>
2022-11-10 23:10:42 +0100

    Adding base project

---
+++ src/main/java/com/pocbackend/PocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocbackend/PocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
